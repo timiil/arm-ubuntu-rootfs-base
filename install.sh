a854817cbda763bd3f9418f8dabbbdce8d03721b
timiil <6621768+timiil@users.noreply.github.com>
2019-11-08 14:59:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,6 +10,6 @@ echo "iface eth0 inet dhcp" >> /etc/network/interfaces.d/eth0
 echo "nameserver 8.8.8.8" > /etc/resolv.conf
 echo "nameserver 114.114.114.114" >> /etc/resolv.conf
 
-apt-get update -y  
-apt-get upgrade -y 
+apt-get update -y 
+apt-get upgrade -y
 apt-get install -y ifupdown net-tools network-manager udev sudo ssh nano
